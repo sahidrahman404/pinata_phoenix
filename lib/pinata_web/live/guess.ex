a4d66a730a7edf6bf85a2e5cbdf405fc84bd0639
Sahid Rahman <sahidrahman404@gmail.com>
2023-01-10 08:15:21 +0700

    add Guess Module for guess route
    this module is responsible for mounting and rendering the live view.
    And for handling events

---
+++ lib/pinata_web/live/guess.ex
@@ -0,0 +1,68 @@
+defmodule PinataWeb.Guess do
+  use Phoenix.LiveView, layout: {PinataWeb.LayoutView, "live.html"}
+
+  def mount(_params, _session, socket) do
+    {:ok,
+     assign(socket,
+       score: 0,
+       message: "Make a guess:",
+       generated_num: :rand.uniform(10),
+       color: ""
+     )}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <%= if @score >= 0 do %>
+      <h1>Your score: <%= @score %></h1>
+      <h2>
+        <p style={"color:#{@color}"}><%= @message %></p>
+        <p>It's <span><%= time() %></span></p>
+      </h2>
+      <h2>
+        <%= for n <- 1..10 do %>
+          <a href="#" phx-click="guess" phx-value-number={n}><%= n %></a>
+        <% end %>
+      </h2>
+      <p>hint: <%= @generated_num %></p>
+    <% else %>
+      <h1 style="color: orange;text-align: center">GAME OVER !!!</h1>
+      <a data-phx-link="patch" data-phx-link-state="push" href="/">
+        Try Again!!!
+      </a>
+    <% end %>
+    """
+  end
+
+  def time() do
+    DateTime.utc_now() |> to_string
+  end
+
+  def handle_event("guess", %{"number" => guess} = data, socket) do
+    {guess, _} = Integer.parse(Map.get(data, "number"))
+    num = socket.assigns.generated_num
+    score = socket.assigns.score
+
+    cond do
+      num == guess ->
+        message = "Your guess #{guess}. Right. Guess again."
+        score = score + 1
+        generated_num = :rand.uniform(10)
+        color = "green"
+
+        {:noreply,
+         assign(socket,
+           message: message,
+           score: score,
+           generated_num: generated_num,
+           color: color
+         )}
+
+      num != guess ->
+        message = "Your guess: #{guess}. Wrong. Guess again."
+        score = score - 1
+        color = "red"
+        {:noreply, assign(socket, message: message, score: score, color: color)}
+    end
+  end
+end
