37f0ebd08a31b4d93b814bbbfe384378aadb1914
Sahid Rahman <sahidrahman404@gmail.com>
2023-01-11 09:59:20 +0700

    add get_user_by_username_and_password function

--- lib/pinata/accounts.ex
+++ lib/pinata/accounts.ex
@@ -44,6 +44,25 @@ defmodule Pinata.Accounts do
     if User.valid_password?(user, password), do: user
   end
 
+
+  @doc """
+  Gets a user by username and password.
+
+  ## Examples
+
+      iex> get_user_by_username_and_password("rbbx574", "correct_password")
+      %User{}
+
+      iex> get_user_by_username_and_password("rbbx574", "invalid_password")
+      nil
+
+  """
+  def get_user_by_username_and_password(username, password)
+      when is_binary(username) and is_binary(password) do
+    user = Repo.get_by(User, username: username)
+    if User.valid_password?(user, password), do: user
+  end
+
   @doc """
   Gets a single user.
 
--- lib/pinata_web/controllers/user_session_controller.ex
+++ lib/pinata_web/controllers/user_session_controller.ex
@@ -9,9 +9,9 @@ defmodule PinataWeb.UserSessionController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    %{"email" => email, "password" => password} = user_params
+    %{"username" => username, "password" => password} = user_params
 
-    if user = Accounts.get_user_by_email_and_password(email, password) do
+    if user = Accounts.get_user_by_username_and_password(username, password) do
       UserAuth.log_in_user(conn, user, user_params)
     else
       # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
