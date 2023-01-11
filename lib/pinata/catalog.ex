de189afa2495962f0ff15846c74ab30f5bd09b0c
Sahid Rahman <sahidrahman404@gmail.com>
2023-01-11 19:48:17 +0700

    add the markdown_product context to catalog

--- lib/pinata/catalog.ex
+++ lib/pinata/catalog.ex
@@ -101,4 +101,10 @@ defmodule Pinata.Catalog do
   def change_product(%Product{} = product, attrs \\ %{}) do
     Product.changeset(product, attrs)
   end
+
+  def markdown_product(%Product{} = product, attrs \\ %{}) do
+    product
+    |> Product.update_price_changeset(attrs)
+    |> Repo.update!()
+  end
 end
