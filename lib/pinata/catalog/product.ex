2e36e546e37086faf908314541b0973c8c1b82ef
Sahid Rahman <sahidrahman404@gmail.com>
2023-01-11 19:47:51 +0700

    add update_price_changeset

--- lib/pinata/catalog/product.ex
+++ lib/pinata/catalog/product.ex
@@ -19,4 +19,19 @@ defmodule Pinata.Catalog.Product do
     |> unique_constraint(:sku)
     |> validate_number(:unit_price, greater_than: 0.0)
   end
+
+  def update_price_changeset(product, attrs) do
+    current_price = product.unit_price
+    product
+    |> cast(attrs, [:id, :unit_price])
+    |> validate_required([:id, :unit_price])
+    |> validate_number(:unit_price, greater_than: 0.0)
+    |> validate_change(:unit_price, fn :unit_price, price ->
+      if price > current_price do
+        [price: "price must be lower than the current price #{current_price}"]
+      else
+        []
+      end
+    end)
+  end
 end
